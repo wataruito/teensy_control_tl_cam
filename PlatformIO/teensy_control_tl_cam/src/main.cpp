aad82b178ef0ab463f667c2ef0d0b8d36324a8ee
Wataru Ito <5068789+wataruito@users.noreply.github.com>
2022-07-27 16:54:09 -0400

    clean up

--- PlatformIO/teensy_control_tl_cam/src/main.cpp
+++ PlatformIO/teensy_control_tl_cam/src/main.cpp
@@ -1,11 +1,12 @@
 /*
-teensy_control_tl_cam
+teensy_control_tl_cam/src/main.cpp
 2022-07-26 Created by: Wataru Ito
 
 Function:
     Generates GPIO output to trigger the cameras.
 Development env:
     PlatformIO/VS Code
+    read the "teensy_control_tl_cam.code-workspace"
 Dependency:
     SimpleCLI   https://github.com/SpacehuhnTech/SimpleCLI
 */
@@ -19,91 +20,79 @@ Relevant commands
     commu_arduino("set_camera -stop")
     commu_arduino("start paradigm -button")
 */
-
 #include <Arduino.h>
 
 // Initialize SimpleCLI command line interface
 #include <SimpleCLI.h>
 SimpleCLI cli;
+Command cmdSetCamera;
 // Command cmdSetVerbose;
 // Command cmdSetParadigm;
 // Command cmdSetTone;
-Command cmdSetCamera;
 // Command cmdStart;
 
-#define TRIG_IN 24                 // input from FreezeFrame
+// parameters for controlling
 #define LED_BLINK_DUR_IN_MILLI 100 // LED blinking at 5 Hz, 100 milliseconds * 2 intervals
 #define SEC_IN_MICRO 1000000.0     // constant: 1 second in microseconds
 #define SERIAL_TIMEOUT 100000      // Serial timeout in milliseconds
-#define TIME_SAMPLING_FREQ 100000  // GPIO input/output at 100k Hz
+#define TIME_SAMPLING_FREQ 100000  // GPIO input/output sampling at 100k Hz
 
-// Global variables and constants
 // digital pin assignment
-const int Sound_B_Cont[7] = {7, 8, 9, 10, 11, 12, 13};
-const int Sound_B_Cont_OutMode[7] = {1, 1, 1, 1, 0, 1, 0};
-const int Sound_B_VOL_Plus = Sound_B_Cont[0];
-const int Sound_B_VOL_Minus = Sound_B_Cont[1];
-const int Sound_B_UG = Sound_B_Cont[2];
-const int Sound_B_RX = Sound_B_Cont[3];
-const int Sound_B_TX = Sound_B_Cont[4];
-const int Sound_B_RST = Sound_B_Cont[5];
-const int Sound_B_ACT = Sound_B_Cont[6];
-
-const int Button[2] = {30, 34};
-const int Button_LED[2] = {32, 36};
-const int Sound_B[8] = {38, 40, 42, 44, 46, 48, 50, 52};
-// const int Camera[3] = {31, 33, 35};
-const int pin_cam = 12;
+#define PIN_CAM 12          // camera trigger generating pin
+#define PIN_TRIG_IN 24      // Control camera start/stop
+#define PIN_LED LED_BUILTIN // LED output pin
 
 // global variables
 bool verboseStatus = true;
 
-// camera
-unsigned long cameraTime[3] = {micros() - 30000, micros() - 30000, micros() - 30000};
-unsigned long cameraExposure = 20000;    // Default exposure time: 20 ms
-unsigned long cameraExpInterval = 33333; // Default fps: 30
-int camerasNumber = 1;
-int cameraStatus = 0;
-bool cameraBool[3] = {false, false, false};
-
+// camera control
 int fps = 30;                            // Default fps: 30
 float exposure = 0.02;                   // Default exposure time: 20 ms
-int gain = 1;                            // Default gain: 1
 float cycle_duration = 1.0 / float(fps); // Default cycle duration: 1/30 sec
 
-unsigned long led_start = 0; // monitor elapsed time after LED starts blinking cycle
-const int led = LED_BUILTIN; // LED pin
-bool led_on = LOW;           // LED status
+int gain = 1; // Default gain: 1
 
-unsigned long fz_simulation_start = 0; // monitor elapsed time after simulation starts
-bool fz_simulation = false;            // flag to indicate whether the simulation is running or not
+bool camera_on = false;            // actual camera status
+bool camera_on_set = false;        // state of camera on/off setting
+unsigned long camera_on_start = 0; // monitor elapsed time after simulation starts
+
+// led control
+unsigned long led_start = 0; // monitor elapsed time after LED starts blinking cycle
+bool led_on = LOW;           // LED status
 
 // Create IntervalTimer object
 //      https://www.pjrc.com/teensy/td_timing_IntervalTimer.html
 IntervalTimer int_timer;
 
-void camera(int i)
+void print_status()
 {
+    Serial.print("Camera status: ");
+    Serial.print(camera_on);
+    Serial.print(", fps: ");
+    Serial.print(fps);
+    Serial.print(", exposure: ");
+    Serial.print(exposure);
+    Serial.println(" s");
+}
 
-    if (cameraBool[i] == true)
+// Check fps and exposure time are valid
+bool check_consistency(int _fps, float _exposure)
+{
+    float _cycle_duration = 1.0 / float(_fps);
+    if (_cycle_duration < _exposure)
     {
-        if ((micros() - cameraTime[i]) > cameraExposure)
-        {
-            digitalWrite(pin_cam, LOW); // turn the LED off by making the voltage LOW
-            cameraBool[i] = false;
-        }
+        Serial.println("Error: exposure time is longer than cycle duration");
+        Serial.println("       fps or exposure is reverted to the original value");
+        print_status();
+        return false;
     }
     else
     {
-        if ((micros() - cameraTime[i]) > cameraExpInterval)
-        {
-            digitalWrite(pin_cam, HIGH); // turn the LED on (HIGH is the voltage level
-            cameraBool[i] = true;
-            cameraTime[i] = micros();
-        }
+        return true;
     }
 }
 
+// Command "set_camera" details
 void set_cameraCallback(cmd *cmdPtr)
 {
     Command cmd(cmdPtr);
@@ -111,64 +100,46 @@ void set_cameraCallback(cmd *cmdPtr)
     bool _set_cameraStart = arg.isSet();
     if (_set_cameraStart)
     {
-        cameraStatus = 1;
-        // Serial.println(cameraStatus);
+        camera_on_set = true;
+        // Serial.println(camera_on_set);
     }
 
     arg = cmd.getArgument("stop");
     bool _set_cameraStop = arg.isSet();
     if (_set_cameraStop)
     {
-        cameraStatus = 0;
-        // Serial.println(cameraStatus);
+        camera_on_set = false;
+        // Serial.println(camera_on_set);
     }
 
     arg = cmd.getArgument("status");
     bool _set_cameraStatus = arg.isSet();
     if (_set_cameraStatus)
     {
-        Serial.print("Camera status: ");
-        Serial.print(cameraStatus);
-        Serial.print(", number: ");
-        Serial.print(camerasNumber);
-        Serial.print(", fps: ");
-        //    Serial.print(cameraExpInterval);
-        //    Serial.print(" : ");
-        Serial.print(1000000 / cameraExpInterval);
-        Serial.print(", exposure: ");
-        Serial.print(cameraExposure / 1000);
-        Serial.println(" ms");
+        print_status();
     }
-
-    arg = cmd.getArgument("cameras_number");
-    int _cameras_number = arg.getValue().toInt();
-    if (_cameras_number != -1)
-        camerasNumber = _cameras_number;
 
     arg = cmd.getArgument("fps");
     int _fps = arg.getValue().toInt();
     if (_fps != -1)
     {
-        cameraExpInterval = long(1000000) / long(_fps);
-        //    Serial.print("_fps: ");
-        //    Serial.print(_fps);
-        //    Serial.print(", cameraExpInterval: ");
-        //    Serial.println(cameraExpInterval);
+        if (check_consistency(_fps, exposure))
+        {
+            fps = _fps;
+            cycle_duration = 1.0 / float(fps);
+            print_status();
+        }
     }
 
     arg = cmd.getArgument("exposure");
-    int _exposure = arg.getValue().toInt();
-    if (_exposure != -1)
+    float _exposure = arg.getValue().toFloat();
+    if (_exposure != -1.0)
     {
-        cameraExposure = long(_exposure) * long(1000);
-        Serial.print("_exposure: ");
-        Serial.print(_exposure);
-        Serial.print(", cameraExposure: ");
-        Serial.println(cameraExposure);
-        if (cameraExposure > cameraExpInterval)
+        if (check_consistency(fps, _exposure))
         {
-            cameraExposure = cameraExpInterval;
-            Serial.println("ERROR: cameraExposure = cameraExpInterval");
+            exposure = _exposure;
+            cycle_duration = 1.0 / float(fps);
+            print_status();
         }
     }
 }
@@ -198,9 +169,8 @@ void command_setup()
     cmdSetCamera.addFlagArgument("start");
     cmdSetCamera.addFlagArgument("stop");
     cmdSetCamera.addFlagArgument("status");
-    cmdSetCamera.addArgument("cameras_number", "-1");
     cmdSetCamera.addArgument("fps", "-1");
-    cmdSetCamera.addArgument("exposure", "-1");
+    cmdSetCamera.addArgument("exposure", "-1.0");
 
     // In case of error
     cli.setOnError(errorCallback);
@@ -220,31 +190,31 @@ void led_blink()
         led_on = LOW;
     }
 
-    digitalWrite(led, led_on);
+    digitalWrite(PIN_LED, led_on);
 }
 
 // IntervalTimer callback function
 void interval_timer_callback()
 {
-    // Monitor TRIG_IN
-    if (digitalRead(TRIG_IN) == HIGH)
+    // Monitor PIN_TRIG_IN
+    if ((digitalRead(PIN_TRIG_IN) == HIGH) || (camera_on_set))
     {
-        if (!fz_simulation)
+        if (!camera_on)
         {
-            fz_simulation_start = micros();
+            camera_on_start = micros();
         }
-        fz_simulation = true;
+        camera_on = true;
     }
     else
     {
-        fz_simulation = false;
+        camera_on = false;
     }
 
     // Scan fz array and set TRIG_OUT
-    if (fz_simulation)
+    if (camera_on)
     {
         unsigned long time_current = micros();
-        float elapsed_time = float(time_current - fz_simulation_start) / SEC_IN_MICRO;
+        float elapsed_time = float(time_current - camera_on_start) / SEC_IN_MICRO;
 
         // Serial.print("time_current_f: ");
         // Serial.println(time_current_f);
@@ -253,23 +223,23 @@ void interval_timer_callback()
         {
             if (elapsed_time < exposure)
             {
-                digitalWrite(pin_cam, HIGH);
-                digitalWrite(led, HIGH);
+                digitalWrite(PIN_CAM, HIGH);
+                digitalWrite(PIN_LED, HIGH);
             }
             else
             {
-                digitalWrite(pin_cam, LOW);
-                digitalWrite(led, LOW);
+                digitalWrite(PIN_CAM, LOW);
+                digitalWrite(PIN_LED, LOW);
             }
         }
         else
         {
-            fz_simulation_start = micros();
+            camera_on_start = micros();
         }
     }
     else
     {
-        digitalWrite(pin_cam, LOW);
+        digitalWrite(PIN_CAM, LOW);
         // digitalWrite(led, LOW);
         led_blink();
     }
@@ -278,11 +248,10 @@ void interval_timer_callback()
 void setup()
 {
     // initialize digital pins.
-    // Cameras
-    pinMode(led, OUTPUT);
-    pinMode(pin_cam, OUTPUT);
-    digitalWrite(pin_cam, LOW);
-    pinMode(TRIG_IN, INPUT);
+    pinMode(PIN_LED, OUTPUT);
+    pinMode(PIN_TRIG_IN, INPUT);
+    pinMode(PIN_CAM, OUTPUT);
+    digitalWrite(PIN_CAM, LOW);
 
     // Initialize the USB serial connection with the PC at 115200 baud
     Serial.setTimeout(SERIAL_TIMEOUT);
@@ -300,7 +269,6 @@ void setup()
 
 void loop()
 {
-
     // Command line interface
     if (Serial.available())
     {
@@ -314,15 +282,4 @@ void loop()
 
         // Serial.print("> ");
     }
-
-    // Generate trigger for three cameras
-    if (cameraStatus)
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            camera(i); // currently 30 ms interval.
-        }
-    }
-
-    // Generate sinusoidal LED driving signals
 }
